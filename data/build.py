35100384e5f62474015db0bad4bef109b38d2aa4
Jørgen Haaland <jorghaal@oppdal.idi.ntnu.no>
2021-04-22 11:52:10 +0200

    Add resize to all images in batch

--- data/build.py
+++ data/build.py
@@ -1,38 +1,48 @@
+from albumentations.augmentations import utils
+from numpy.lib.arraypad import pad
 from torch.utils.data import DataLoader, random_split, Subset
 from data.transforms import build_transforms
 from data.DatasetLoader import DatasetLoader
 from pathlib import Path
 from copy import copy
 import torch
+import numpy as np
+from utils.torch_utils import to_cuda
 
 def custom_collate(batch):
     # Credits: https://discuss.pytorch.org/t/how-to-create-a-dataloader-with-variable-size-input/8278/3
     data = [item[0] for item in batch]
     target = [item[1] for item in batch]
-    shapes = [item[0][0].shape for item in batch]
+    shapes = [item[0].shape for item in batch]
     ## get sequence lengths
     max_len = max(shapes)
-    features = torch.zeros((len(data), *max_len))
-    print('Festures size: ', features.shape)
+   
+    images = np.zeros((len(data), *max_len))
+    targets = np.zeros((len(data), *max_len[1:]))
     #labels = torch.tensor(labels)
     shapes = torch.tensor(shapes)
 
     for i in range(len(data)):
         j, k = data[i][0].shape
-        print('Original shape: ', data[i][0].shape)
-        print('Cat x: ', max_len[0]-j)
-        print('Cat y:', max_len[1]-k)
-        print('Final shape: ', data[i][0].shape[0]+max_len[0]-j, data[i][0].shape[1]+max_len[1]-k)
-        print('Max shape: ', max_len)
-        features[i] = torch.cat([data[i][0], torch.zeros((max_len[0]-j, k))])
-        print('Final 2: ', features[i].shape)
+        pad_x = max_len[1] - j
+        pad_y = max_len[2] - k
+
+        padded_img = np.vstack((data[i][0], np.zeros((pad_x, k))))
+        padded_target = np.vstack((target[i], np.zeros((pad_x, k))))
+        padded_target = np.hstack((padded_target, np.zeros((padded_img.shape[0], pad_y))))
+        padded_img = np.hstack((padded_img, np.zeros((padded_img.shape[0], pad_y))))
+        images[i][0] = padded_img
+        targets[i] = padded_target
+        
     
     """
     Should return a tensor, but not possible when the sizes are different...
     https://discuss.pytorch.org/t/how-to-create-batches-of-a-list-of-varying-dimension-tensors/50773/14
 
     """
-    return features.float(), targets.long(), shapes.long()
+    images = torch.from_numpy(images)
+    targets = torch.from_numpy(targets)
+    return images.float(), shapes.long()
 
 def make_data_loaders(cfg, classes=[1, 2], is_train=True, model_depth=False):
     train_transform = build_transforms(cfg, is_train=True)
@@ -60,19 +70,22 @@ def make_data_loaders(cfg, classes=[1, 2], is_train=True, model_depth=False):
                                        num_workers=cfg.DATA_LOADER.NUM_WORKERS,
                                        pin_memory=cfg.DATA_LOADER.PIN_MEMORY,
                                        batch_size=batch_size,
-                                       shuffle=True,)
+                                       shuffle=True,
+                                       collate_fn=custom_collate)
 
         val_data_loader = DataLoader(valid_dataset,
                                      num_workers=cfg.DATA_LOADER.NUM_WORKERS,
                                      pin_memory=cfg.DATA_LOADER.PIN_MEMORY,
                                      batch_size=batch_size,
-                                     shuffle=True,)
+                                     shuffle=True,
+                                     collate_fn=custom_collate)
 
         test_data_loader = DataLoader(test_dataset,
                                       num_workers=cfg.DATA_LOADER.NUM_WORKERS,
                                       pin_memory=cfg.DATA_LOADER.PIN_MEMORY,
                                       batch_size=batch_size,
-                                      shuffle=True,)
+                                      shuffle=True,
+                                      collate_fn=custom_collate)
         return train_data_loader, val_data_loader, test_dataset
     else:
         test_data_loader = DataLoader(test_dataset,
